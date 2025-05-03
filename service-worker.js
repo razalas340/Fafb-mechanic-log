95c72ace7b1fcb257a5a4e0a52e26d9b749a6a89
razalas340 <135702134+razalas340@users.noreply.github.com>
2025-05-03 19:15:25 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('faf-log').then(cache => {
+    return cache.addAll(['.', 'index.html', 'manifest.json', 'icon-192.png']);
+  }));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
